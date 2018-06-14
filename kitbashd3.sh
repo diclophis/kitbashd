c25c302be3cca5c1620f03bdb0c015366d9063aa
Jon Bardin <diclophis@gmail.com>
2018-06-14 04:52:30 -0700

    better boot up args

--- kitbashd3.sh
+++ kitbashd3.sh
@@ -5,8 +5,20 @@ set -x
 
 DISK=$1
 UUID=$2
+
 KERNEL=tmp/vmlinuz-4.13.0-43-generic
 INITRD=tmp/initrd.img-4.13.0-43-generic
+
+#NOTE: broken
+#KERNEL=tmp/vmlinuz-4.13.0-36-generic
+#INITRD=tmp/extras.img
+
+#NOTE: busybox
+#KERNEL=tmp/linux
+#INITRD=tmp/rootfs-empty.gz
+
+#tmp/extras/boot/vmlinuz-4.13.0-36-generic
+#INITRD=tmp/initrd-2.0b.gz
 
 #if [ test -z "${DISK}" || ! test -e "${DISK}" ];
 #then
@@ -20,8 +32,10 @@ then
   exit 1
 fi
 
-IMPORTANT_ARGS="rw root=/dev/vda1 console=ttyS0,1152000n8 systemd.journald.forward_to_console=1"
-CMDLINE="linux ${IMPORTANT_ARGS}"
+#IMPORTANT_ARGS="rw root=/dev/vda1 console=ttyS0,1152000n8 systemd.journald.forward_to_console=1"
+#IMPORTANT_ARGS="rw root=/dev/vda1 console=tty0 console=ttyS0,115200n8 net.ifnames=0 biosdevname=0 modules=virtio_blk,af_socket,loop,squashfs,sd-mod,usb-storage,sr-mod,ext4 debug_init"
+IMPORTANT_ARGS="rw root=/dev/vda1 console=tty0 console=ttyS0,115200n8 net.ifnames=0 biosdevname=0"
+CMDLINE="linux ${IMPORTANT_ARGS} init=/bin/init"
 
 MEM="-m 3G"
 CPU="-c 3"
